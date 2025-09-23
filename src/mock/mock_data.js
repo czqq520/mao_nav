3919d5b32a665be6cec4c8367cf16b46a33d1ab8
czqq520 <czqczqczq520@gmail.com>
2025-09-23 19:26:54 +0800

    chore: 更新导航数据 - 2025/9/23 19:27:03

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -10,7 +10,8 @@ export const mockData = {
           "id": "site-1752857783656",
           "name": "网盘拉新【陪跑】",
           "url": "https://yuntui.huazhuanapp.cn/#/pages/pan-news?accId=8XA3D&inviterCode=PN9D67",
-          "description": "网盘拉新，副业陪跑。"
+          "description": "网盘拉新，副业陪跑。",
+          "icon": "https://ainb.us/logo.png"
         },
         {
           "id": "linux-do",
